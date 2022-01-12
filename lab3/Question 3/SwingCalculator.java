eaad109ef44b49152440b27b4e5270fece527540
prashant1124 <59989100+prashant1124@users.noreply.github.com>
2022-01-12 22:14:06 +0545

    Add files via upload

---
+++ lab3/Question 3/SwingCalculator.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class SwingCalculator
+{
+    SwingCalculator()
+    {
+        JFrame frame = new JFrame();
+
+        JLabel label1 = new JLabel("Number 1: "); 
+        label1.setBounds(30,40,70,50);
+        JTextField tf1 = new JTextField();
+        tf1.setBounds(110,50,150,30);
+
+        JLabel label2 = new JLabel("Number 2: "); 
+        label2.setBounds(30,70,70,100);
+        JTextField tf2 = new JTextField();
+        tf2.setBounds(110,100,150,30);
+
+        JLabel label3 = new JLabel("Answer: "); 
+        label3.setBounds(30,150,70,100);
+        JTextField tf3 = new JTextField();
+        tf3.setBounds(110,140,150,30);
+
+        JButton addButton = new JButton("Add");
+        addButton.setBounds(40,200,80,50);
+
+        JButton subButton = new JButton("Subtract");
+        subButton.setBounds(110,200,20,30);
+
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(tf1);
+        frame.add(tf2);
+        frame.add(tf3);
+        frame.add(addButton);
+        frame.add(subButton);
+
+        addButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				tf3.setText(""+(Integer.parseInt(tf1.getText())+Integer.parseInt(tf2.getText())));
+			}
+		});
+
+		subButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				tf3.setText(""+(Integer.parseInt(tf1.getText())-Integer.parseInt(tf2.getText())));
+			}
+		});
+
+        frame.setSize(700,700);
+        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+    }
+
+    public static void main(String[] args)
+    {
+        new SwingCalculator();
+    }
+}
---
+++ lab3/README.md
@@ -0,0 +1,15 @@
+# LifeCycle method in Applet
+There are 5 lifecycle method in Applet. They are: init(), start(), paint(), stop() & destroy(). 
+There are 4 life cycle states they are born, running, idle & dead. init() is run before the applet is born.
+ start() is run immediately after the applet is born. paint() method is used to draw graphics in applet. 
+ stop() method is run when the applet goes to idle state. destroy() method is called before the applet dies.
+
+# Passing parameter in Applet
+We can pass down parameter in our applet program from the html code using the <param> tag. 
+It has attributes like name which specifies the name of the paramter & value attribute provides the value for 
+the respective param.
+
+# Action Listener in Java
+ActionListener in Java is a class that is responsible for handling all action events such as when the
+ user clicks on a component. Mostly, action listeners are used for JButtons. An ActionListener can be 
+ used by the implements keyword to the class definition.
